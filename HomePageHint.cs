d55068889dcac2a82519a27fdaa967fad8add5de
mjwhhhh <jiawei@unitygames.cn>
2014-09-16 14:43:34 +0800

    提示语控制类
    控制提示语的弹出与文字显示

---
+++ HomePageHint.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HomePageHint : MonoBehaviour {
+
+    /*
+     * 首页提示，用于显示及消失教程气泡框
+     * */
+	GameObject thisG;
+	Transform thisT;
+	public UILabel WordLabel;
+	public UIButtonTween ScaleBtn;
+	public Transform BackImgT;
+	public UISprite BackImg;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		thisG = this.gameObject;
+		thisT = this.transform;
+		thisG.SetActive(false);
+	}
+
+	public void SetLabelWord(string Word)
+	{
+		WordLabel.text = Word;
+	}
+
+	public void ScaleUp()
+	{
+		if(!thisG.activeSelf)
+		  thisG.SetActive(true);
+		ScaleBtn.Play(true);
+	}
+
+	public void ReverseBackScaleX()
+	{
+		BackImg.enabled = false;
+		BackImgT.localScale = new Vector3(-BackImgT.localScale.x,BackImgT.localScale.y,BackImgT.localScale.z);
+		BackImg.enabled = true;
+	}
+}
